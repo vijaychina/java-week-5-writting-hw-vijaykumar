6556b2e78a793015d7a9f8fde067ee7c8820ac95
vijaychina <vijaykumar.china5@gmail.com>
2023-11-15 17:39:41 +0000

    java-week-5-writting-hw-vijaykumar

---
+++ src/Programme_3_ReverseArray.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+/**
+ * Created by Vijaykumar China
+ */
+/*
+Write a Java program to reverse an array of integer values
+ */
+
+public class Programme_3_ReverseArray {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{23, 45, 34, 54, 67, 78}; //Single dimension array
+        System.out.println("Original array: " + Arrays.toString(array));
+        Programme_3_ReverseArray obj = new Programme_3_ReverseArray();
+        System.out.print("Reverse array : ");
+        obj.reverseAnArray(array);
+    }
+
+    // method to reverse an array
+    public void reverseAnArray(int[] list) {
+
+        for (int i = 0; i < (list.length) / 2; i++) {
+            int temp = list[i];
+            list[i] = list[list.length - 1 - i];
+            list[list.length - 1 - i] = temp;
+        }
+        System.out.println(Arrays.toString(list));
+    }
+}
---
+++ src/Programme_4_ColourArrayList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Vijaykumar China
+ */
+/*
+Write a Java program to create a new array list, add some colours (string)
+and printout the collection using for each loop.
+ */
+
+public class Programme_4_ColourArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> colorList = new ArrayList<>();
+        colorList.add("RED");
+        colorList.add("BLUE");
+        colorList.add("GREEN");
+        colorList.add("YELLOW");
+        colorList.add("BLACK");
+        colorList.add("WHITE");
+        colorList.add("ORANGE");
+        colorList.add("PURPLE");
+        colorList.add("CYAN");
+
+        for (String colourList : colorList) {
+            System.out.print(colourList + ", ");
+        }
+    }
+}
